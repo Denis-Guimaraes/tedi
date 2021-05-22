b1405c2179b7eb6bb1a3eb6d3080fd214b5a3a0a
Denis Guimaraes <guimaraes.dev@gmail.com>
2021-05-22 12:46:18 +0200

    add flags to extract command

--- cmd/extract.go
+++ cmd/extract.go
@@ -6,15 +6,47 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var Folder string
+var Pattern []string
+var Output string
+
 var extractCmd = &cobra.Command{
 	Use:   "extract",
 	Short: "Extract text from files",
 	Long:  "Extract text from files.",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Todo")
+		fmt.Println(Folder)
+		for _, p := range Pattern {
+			fmt.Println(p)
+		}
+		fmt.Println(Output)
 	},
 }
 
 func init() {
+	extractCmd.Flags().StringVarP(
+		&Folder,
+		"folder",
+		"f",
+		"./",
+		"folder path to scan",
+	)
+
+	extractCmd.Flags().StringSliceVarP(
+		&Pattern,
+		"pattern",
+		"p",
+		[]string{"\\{te\\}([^}]*)\\{te\\}"},
+		"regular expression to extract texts",
+	)
+
+	extractCmd.Flags().StringVarP(
+		&Output,
+		"output",
+		"o",
+		"stdout",
+		"output type, stdout or csv",
+	)
+
 	rootCmd.AddCommand(extractCmd)
 }
