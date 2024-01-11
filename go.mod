68320739a4ea7bbddd3c8d155de76c9d00e6908b
denis <denis@6tematik.fr>
2024-01-11 10:24:50 +0100

    update package + ajout validation

--- go.mod
+++ go.mod
@@ -2,4 +2,12 @@ module local/tedi
 
 go 1.16
 
-require github.com/spf13/cobra v1.1.3
+require (
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
+	github.com/spf13/cobra v1.8.0
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/spf13/viper v1.7.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
--- src/replacer/replacer.go
+++ src/replacer/replacer.go
@@ -27,7 +27,10 @@ func New(path []string, ignore []string, delimiter string, csv string) *replacer
 
 func (r *replacer) ReplaceAll() {
 	lines := r.readCsv()
-	for _, line := range lines {
+	for index, line := range lines {
+		if !r.isValideLine(line, index+2) {
+			continue
+		}
 		r.replace(line)
 	}
 }
@@ -36,7 +39,7 @@ func (r *replacer) readCsv() [][]string {
 	file := r.openCsv()
 	lines, err := csv.NewReader(file).ReadAll()
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "error read csv: %v\n", err)
+		fmt.Fprintf(os.Stderr, "error read csv %s: %v\n", r.csv, err)
 		os.Exit(1)
 	}
 
@@ -47,10 +50,26 @@ func (r *replacer) readCsv() [][]string {
 func (r *replacer) openCsv() *os.File {
 	file, err := os.Open(r.csv)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "error open csv: %v\n", err)
+		fmt.Fprintf(os.Stderr, "error open csv %s: %v\n", r.csv, err)
 		os.Exit(1)
 	}
 	return file
+}
+
+func (r *replacer) isValideLine(line []string, lineNumber int) bool {
+	if len(line[0]) == 0 {
+		fmt.Fprintf(os.Stdout, "line %d: file column empty\n", lineNumber)
+		return false
+	}
+	if len(line[1]) == 0 {
+		fmt.Fprintf(os.Stdout, "line %d: text column empty\n", lineNumber)
+		return false
+	}
+	if len(line[2]) == 0 {
+		fmt.Fprintf(os.Stdout, "line %d: replace column empty\n", lineNumber)
+		return false
+	}
+	return true
 }
 
 func (r *replacer) replace(line []string) {
@@ -62,6 +81,11 @@ func (r *replacer) replace(line []string) {
 		}
 
 		oldText := fmt.Sprintf("%s%s%s", r.delimiter, line[1], r.delimiter)
+		if !strings.Contains(string(content), oldText) {
+			fmt.Fprintf(os.Stdout, "note found text %s in file %s\n", oldText, fp)
+			continue
+		}
+
 		newContent := strings.ReplaceAll(string(content), oldText, line[2])
 		r.WriteFile(fp, newContent)
 	}
@@ -70,7 +94,7 @@ func (r *replacer) replace(line []string) {
 func (r *replacer) readFile(path string) []byte {
 	text, err := os.ReadFile(path)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "error read file: %v\n", err)
+		fmt.Fprintf(os.Stderr, "error read file %s: %v\n", path, err)
 		return nil
 	}
 	return text
@@ -79,7 +103,7 @@ func (r *replacer) readFile(path string) []byte {
 func (r *replacer) WriteFile(path string, content string) {
 	err := os.WriteFile(path, []byte(content), 0)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		fmt.Fprintf(os.Stderr, "error write file %s: %v\n", path, err)
 		return
 	}
 	fmt.Fprintf(os.Stdout, "content changed in %s\n", path)
